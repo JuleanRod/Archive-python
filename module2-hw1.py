a3d05d418d2c92d7d21aee6c1dc79cf8f270353b
Julian <92952502+juleanrod@users.noreply.github.com>
2020-02-28 11:11:30 -0800

    anual salary comparison

---
+++ module2-hw1.py
@@ -0,0 +1,19 @@
+total_salary = 0
+salary = 0
+
+salary = int( input("Please enter first person's yearly salary: "))
+total_salary += salary
+print("The total salary so far is: " + str(total_salary))
+salary = int( input("Please enter second person's yearly salary: "))
+total_salary += salary
+print("The total salary so far is: " + str(total_salary))
+salary = int( input("Please enter third person's yearly salary: "))
+total_salary += salary
+print("The total salary so far is: " + str(total_salary))
+salary = int( input("Please enter fourth person's yearly salary: "))
+total_salary += salary
+print("The total salary so far is: " + str(total_salary))
+salary = int( input("Please enter fifth person's yearly salary: "))
+total_salary += salary
+
+print("Total salary so far is: " + str(total_salary))
